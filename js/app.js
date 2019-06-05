0eaa5adfe7f0669401769856f03f5af81a075291
jkelley399 <jkelley@399innovation.com>
2019-06-05 16:13:43 -0700

    feat player movement with boundaries
    Took me a while to figure out what was being returned from
    document.addEventListener('keyup', function(e) to
    A key insight was provided by
    https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#The_event_listener_callback
    
    TODO:   collisonTest()
            victoryTest()

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-// NOTE: Assuming x-values (columns) precede y-values and
+// NOTE 2019-06-03: Assuming x-values (columns) precede y-values and
 // constants need to be added based on engine.js function render() @105/137:
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y *83);
@@ -86,40 +86,59 @@ Player.prototype.update = function(dt) {
     // all computers.
 };
 
+
+// helper function for collisonTest();
+function logName(element) {
+    console.log(element);
+}
+
 // Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
 // NOTE: For dt, see engine.js function main() @34:
 // NOTE: Outline essentially copied from base code for Enemy
-Player.prototype.handleInput = function() {
+Player.prototype.handleInput = function(event) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 
-// document.addEventListener('keyup', function(e) {
-//     console.log(e.keyCode);
-//     player.currentKeyupValue = e.keyCode;
-// });
+    let keyDirection = event;
+    const allowedDirections = ['left', 'up', 'right', 'down'];
 
-    let keyNumber = player.currentKeyupValue;
-    // let keyNumber = document.addEventListener.onkeyup;
-    console.log(keyNumber);
-
-    if ((keyNumber == 'ArrowLeft') || (keyNumber == 37)) {
+    // 2019-06-05: first if () test based on
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+    if (allowedDirections.includes(keyDirection) === false) {
+        console.log(keyDirection + " from eventListener was improper input");
+    } else if ((keyDirection == 'left') && (player.x >= 0.75)) {
         player.x -= 1;
-    } else if ((keyNumber == 'ArrowUp') || (keyNumber == 38)) {
+    } else if ((keyDirection == 'up') && (player.y >= 0)) {
         player.y -= 1;
-    } else if ((keyNumber == 'ArrowRight') || (keyNumber == 39)) {
+    } else if ((keyDirection == 'right') && (player.x <= 3)) {
         player.x += 1;
-    } else if ((keyNumber == 'ArrowDown') || (keyNumber == 40)) {
+    } else if ((keyDirection == 'down') && (player.y <= 4)) {
         player.y += 1;
     } else {
-        console.log(keyNumber);
+        console.log("Player has reached an x or y limit.");
     }
 
+    // collisonTest();
+    const collisonK = 0.5;
+
+    // 2019-06-05: Basic syntax from
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+    allEnemies.forEach(logName);
+
+    // for 3 rows
+    //     for all enemies in row
+    //         if abs(player.x - enemy.x) < collisonK
+    //             return true;
+    //         else
+    //             return false;
+
+    // victoryTest();
 };
 
 // Draw the player on the screen, required method for game
-// NOTE: Essentially copied from base code for Enemy
+// NOTE 2019-06-03: Essentially copied from base code for Enemy
 // NOTE: Assuming x-values (columns) precede y-values and
 // constants need to be added based on engine.js function render() @105/137:
 Player.prototype.render = function() {
@@ -137,7 +156,7 @@ const enemy2 = new Enemy(2, 0, 0.9);
 const enemy3 = new Enemy(3, 0, 1.5);
 const enemy4 = new Enemy(1, 0, 2.0);
 
-allEnemies = [enemy1, enemy2, enemy3, enemy4];
+const allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -152,15 +171,72 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// TODO: Should be a way to avoid this duplication of eventListeners
-document.addEventListener('keyup', function(e) {
-    console.log(e.keyCode);
-    player.currentKeyupValue = e.keyCode;
-});
+/* TODO
+
+*/
 
 /* ADDITIONAL REFERENCES CONSULTED
+
+Javascript:
+
 https://developer.mozilla.org/en-US/docs/Web/API/Document/keyup_event
+https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#The_event_listener_callback
 
+    "The event listener can be specified as either a callback function or
+    an object that implements EventListener, whose handleEvent() method serves
+    as the callback function.
 
+    The callback function itself has the same parameters and return value as
+    the handleEvent() method; that is, the callback accepts a single
+    parameter: an object based on Event describing the event which has
+    occurred, and it returns nothing."
 
+https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onkeyup
+https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Event_handlers
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+*/
+
+/* KEY LEARNING
+
+2019-06-04--2019-06-05:
+Took me a while to figure out what was being returned from
+document.addEventListener('keyup', function(e) to
+A key insight was provided by
+https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#The_event_listener_callback
+
+    "The callback function itself has the same parameters and return value as
+    the handleEvent() method; that is, the callback accepts a single
+    parameter: an object based on Event describing the event which has
+    occurred, and it returns nothing."
+
+I added an explicit parameter, "event," to
+    Player.prototype.handleInput = function(event)
+After doing that, I checked the return values in two different console.log statements,
+one in Player.prototype.handleInput = function(event) and
+one in document.addEventListener('keyup', function(e)
+
+Surprisingly, they provided two different results (see the following excerpts from
+Console in Chrome Dev Tools):
+
+"left
+app.js:180 KeyboardEvent {isTrusted: true, key: "ArrowLeft", code: "ArrowLeft",
+location: 0, ctrlKey: false, …}"
+
+In was apparent from the comparison, however, that the directional strings,
+'left,' 'right,' etc. were reaching Player.prototype.handleInput = function(event),
+and, in addtion to the explicit paramater, "event," that was enough to write the
+player movement code.
+
+I still don't understand why the two console.log statements produce different results; or
+but I'm beginning to understand how (a) var allowedKeys and
+(b) player.handleInput(allowedKeys[e.keyCode]) together operate to both (1) filter
+the acceptable keyboard input and (2) translate the keyCode for the key whose release
+fires the eventListener into a more comprehensible string that can be used in
+Player.prototype.handleInput = function(event)
 */
